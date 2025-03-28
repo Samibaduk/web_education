d34cefb8b137364a5c9fbf881276fc7fa4759d47
Smart developer <kostya.vit.99@mail.ru>
2025-03-28 10:07:43 +0200

    Варианты выбора

--- mars/mars.py
+++ mars/mars.py
@@ -166,5 +166,37 @@ def astro():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def planet_n(planet_name):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                  </head>
+                  <body>
+                  <h1>Мое предложение: {planet_name}</h1>
+                  <h5>Это планета близка к Земле;</h5>
+                    <div class="alert alert-success" role="alert">
+                      На ней много необходимых ресурсов;
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      На ней есть вода и атмосфера;
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      На ней есть небольшое магнитное поле;
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Наконец, она просто красива!
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

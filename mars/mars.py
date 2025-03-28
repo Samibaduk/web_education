b758d2139967f953cb1b958a07813b3a9a438e18
Smart developer <kostya.vit.99@mail.ru>
2025-03-28 12:09:20 +0200

    Загрузка файла

--- mars/mars.py
+++ mars/mars.py
@@ -1,4 +1,6 @@
+import io
 from flask import Flask, url_for, request
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -224,6 +226,43 @@ def results(nickname, level, rating):
                     </div>
                   </body>
                 </html>"""
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <h1 align="center">Загрузка фотографии</h1>
+                            <h3 align="center">для участия в миссии</h3>
+                            <form class="login_form" method="post" enctype="multipart/form-data">
+                               <div class="form-group">
+                                    <label for="photo">Приложите фотографию</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                               </div>
+                               <br></br>
+                               <img src="{url_for('static', filename='img/profile.png')}"
+                                alt="здесь должна была быть картинка, но не нашлась">
+                                <br><button type="submit" class="btn btn-primary">Отправить</button></br>
+                            </form>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        f = io.BytesIO(request.files['file'].read())
+        img = Image.open(f)
+        img.save('static/img/profile.png', 'PNG')
+        return 'Откройте форму снова чтобы увидеть картинку'
 
 
 if __name__ == '__main__':

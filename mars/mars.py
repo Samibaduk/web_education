f3346fb8096bc4cec8e4a876552c38d6c4a01a6f
Smart developer <kostya.vit.99@mail.ru>
2025-03-28 16:38:24 +0200

    Пейзажи Марса

--- mars/mars.py
+++ mars/mars.py
@@ -265,5 +265,52 @@ def load_photo():
         return 'Откройте форму снова чтобы увидеть картинку'
 
 
+@app.route('/carousel')
+def carousel():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пейзажи Марса</title>
+                          </head>
+                          <body>
+                            <h1 align="center">Пейзажи Марса</h1>
+                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img class="d-block w-100" src="{url_for('static', filename='img/car1.jpg')}" alt="Первый слайд">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="{url_for('static', filename='img/car2.jpg')}" alt="Второй слайд">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="{url_for('static', filename='img/car3.jpg')}" alt="Третий слайд">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="{url_for('static', filename='img/car4.jpg')}" alt="Четвертый слайд">
+                                </div>
+                              </div>
+                              <button class="carousel-control-prev" href="#carouselExampleControls" type="button" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Предыдущий</span>
+                              </button>
+                              <button class="carousel-control-next" href="#carouselExampleControls" type="button" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Следующий</span>
+                              </button>
+                            </div>
+                            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+                            </form>
+                          </body>
+                        </html>'''
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
